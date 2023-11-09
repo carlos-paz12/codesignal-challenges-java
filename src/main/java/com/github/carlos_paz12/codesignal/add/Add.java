86c5b92eb786ae60aae5b2ce232cdad7abb82554
Carlos Paz <147278027+carlos-paz12@users.noreply.github.com>
2023-11-09 13:53:45 -0300

    feat: Add 'add' challenge

---
+++ src/main/java/com/github/carlos_paz12/codesignal/add/Add.java
@@ -0,0 +1,12 @@
+package com.github.carlos_paz12.codesignal.add;
+
+public class Add {
+
+    /*
+     * Write a function that returns the sum of two numbers.
+     */
+    int solution(int param1, int param2) {
+        return param1 + param2;
+    }
+
+}
